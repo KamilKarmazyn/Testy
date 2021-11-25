a1b2d77860927fa58406e864598187726b5e7d19
MSI\kamil <kamilkarmazyn303@gmail.com>
2021-11-25 16:36:23 +0100

    TDD sam test próba 2

---
+++ TDD/src/TDDPackage/TestTDD.java
@@ -0,0 +1,29 @@
+package TDDPackage;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class TestTDD {
+
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@Test
+	void test() {
+		
+		switch (b)
+		{
+		    case "dobrze":
+		    {
+		    	System.out.println("No to dobrze");
+		    	assertTrue(true);
+		    }
+		    default:
+		   	{
+		   		assertFalse(true);
+		    }	
+}
+}}
