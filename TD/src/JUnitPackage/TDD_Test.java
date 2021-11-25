2e0c03f51d81e153e891e9431f4f60e67ff4b5bf
MSI\kamil <kamilkarmazyn303@gmail.com>
2021-11-25 16:09:56 +0100

    TDD sam test

---
+++ TD/src/JUnitPackage/TDD_Test.java
@@ -0,0 +1,28 @@
+package JUnitPackage;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.Scanner;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class TDD_Test {
+	@Test
+	void test() 
+	{
+		
+		switch (b)
+		{
+		    case "dobrze":
+		    {
+		    	System.out.println("No to dobrze");
+		    	assertTrue(true);
+		    }
+		    default:
+		   	{
+		   		assertFalse(false);
+		    }	
+		}}}
+
