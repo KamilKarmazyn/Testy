ff4e877d22cfc87628671fec444a4e961ba2d3dd
MSI\kamil <kamilkarmazyn303@gmail.com>
2021-11-25 16:37:57 +0100

    Test z aplikacją

--- TDD/src/TDDPackage/TestTDD.java
+++ TDD/src/TDDPackage/TestTDD.java
@@ -2,13 +2,21 @@ package TDDPackage;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.Scanner;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class TestTDD {
+	public String b;
 
 	@BeforeEach
-	void setUp() throws Exception {
+	void setUp() throws Exception 
+	{
+	    Scanner scan = new Scanner(System.in);
+	    System.out.println("Wpisz dobrze");
+	    String a = scan.nextLine();
+	    b = a;
 	}
 
 	@Test
